ebe2ebf1bf22e19af197b88937c8442c96e37e4c
Ujjal Dey <ujjaldey@gmail.com>
2023-03-31 16:06:55 +0800

    Video 101

--- src/main/scala/fpfinal/model/Money.scala
+++ src/main/scala/fpfinal/model/Money.scala
@@ -3,7 +3,7 @@ package fpfinal.model
 import cats._
 import cats.implicits._
 import fpfinal.app.Configuration.IsValid
-import fpfinal.common.Validations._
+import fpfinal.common.Validations.nonNegative
 import fpfinal.model.Money.showMoney
 
 import scala.util.Try
@@ -15,7 +15,7 @@ import scala.util.Try
  *
  * @param _cents the amount in cents
  */
-class Money private (_cents: Int) {
+class Money private(_cents: Int) {
   /**
    * @return the amount expressed in cents
    */
@@ -45,13 +45,13 @@ class Money private (_cents: Int) {
   def times(n: Int): Money = new Money(_cents * n)
 
   /**
-    * TODO #6: Implement division by an integer. When n is 0, the result should be None.
-    * Otherwise, it should be a Some.
-    *
-    * For simplicity we don't care about losing cents. For example, dividing 1 dollar
-    * by 3 should yield 33 cents.
-    */
-  def divideBy(n: Int): Option[Money] = ???
+   * TODONE #6: Implement division by an integer. When n is 0, the result should be None.
+   * Otherwise, it should be a Some.
+   *
+   * For simplicity we don't care about losing cents. For example, dividing 1 dollar
+   * by 3 should yield 33 cents.
+   */
+  def divideBy(n: Int): Option[Money] = Try(new Money(_cents / n)).toOption
 
   /**
    * @return a string representation of this money amount in dollars, with two decimal places
@@ -72,12 +72,15 @@ object Money {
   val zero: Money = new Money(0)
 
   /**
-    * TODO #7: Create a validated Money object that represents this dollars amount.
-    * The only validation to perform is:
-    * - Amount should be non-negative
-    */
+   * TODONE #7: Create a validated Money object that represents this dollars amount.
+   * The only validation to perform is:
+   * - Amount should be non-negative
+   */
   def dollars(amount: Double): IsValid[Money] =
-    ???
+    nonNegative(amount).map { dls =>
+      val cents = (dls * 100).toInt
+      new Money(cents)
+    }
 
   implicit val monoidMoney: Monoid[Money] = Monoid.instance(zero, _ plus _)
 
@@ -87,8 +90,8 @@ object Money {
     Eq.instance((m1, m2) => m1.cents === m2.cents)
 
   /**
-    * TODO #4: Implement and instance of Order for Money that compares its cents.
-    * Use the given Order instance for comparing any Int values.
-    */
-  implicit def orderMoney(implicit orderInt: Order[Int]): Order[Money] = ???
+   * TODONE #4: Implement and instance of Order for Money that compares its cents.
+   * Use the given Order instance for comparing any Int values.
+   */
+  implicit def orderMoney(implicit orderInt: Order[Int]): Order[Money] = Order.by(_.cents)
 }
