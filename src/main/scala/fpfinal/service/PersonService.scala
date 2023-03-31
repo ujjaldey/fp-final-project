181f9e0f2e61a3e65157608d3ba70e35d2a1f6f7
Ujjal Dey <ujjaldey@gmail.com>
2023-03-31 18:13:24 +0800

    Video 105

--- src/main/scala/fpfinal/service/PersonService.scala
+++ src/main/scala/fpfinal/service/PersonService.scala
@@ -7,53 +7,57 @@ import fpfinal.model.Person
 import fpfinal.service.PersonService.PersonOp
 
 /**
- * A trait for adding, storing and then finding people.
- *
- * It uses the State monad to keep track of the people that the user of the application
- * has added so far.
- */
+  * A trait for adding, storing and then finding people.
+  *
+  * It uses the State monad to keep track of the people that the user of the application
+  * has added so far.
+  */
 trait PersonService {
   import PersonService._
 
   val personService: Service
 
   trait Service {
+
     /**
-     * Finds a person with the given name, returning if it finds it in the state,
-     * or yielding None otherwise.
-     */
+      * Finds a person with the given name, returning if it finds it in the state,
+      * or yielding None otherwise.
+      */
     def findByName(name: String): PersonOp[Option[Person]]
 
     /**
-     * Adds a person to the state.
-     */
+      * Adds a person to the state.
+      */
     def addPerson(person: Person): PersonOp[Unit]
 
     /**
-     * Returns all the people in the state.
-     */
+      * Returns all the people in the state.
+      */
     def getAllPeople(): PersonOp[List[Person]]
   }
 }
 
 object PersonService {
+
   /**
-   * Represents a state containing people indexed by name.
-   *
-   * @param personByName a map with names as keys and the associated Person instance as values.
-   */
+    * Represents a state containing people indexed by name.
+    *
+    * @param personByName a map with names as keys and the associated Person instance as values.
+    */
   case class PersonState(personByName: Map[String, Person]) {
+
     /**
-     * @return a new state with the given person added
-     */
+      * @return a new state with the given person added
+      */
     def addPerson(person: Person): PersonState =
       copy(personByName = personByName + (person.name -> person))
   }
 
   object PersonState {
+
     /**
-     * A state with no people.
-     */
+      * A state with no people.
+      */
     def empty: PersonState = PersonState(Map.empty)
 
     implicit def eqPersonState(implicit
@@ -66,24 +70,28 @@ object PersonService {
 }
 
 /**
-  * TODO #22: Provide a LivePersonService with an implementation for PersonService.
+  * TODONE #22: Provide a LivePersonService with an implementation for PersonService.
   */
 trait LivePersonService extends PersonService {
   override val personService: Service = new Service {
-    /**
-     * Finds a person with the given name, returning it if it finds it in the state,
-     * or yielding None otherwise.
-     */
-    override def findByName(name: String): PersonOp[Option[Person]] = ???
 
     /**
-     * Adds a person to the state.
-     */
-    override def addPerson(person: Person): PersonOp[Unit] = ???
+      * Finds a person with the given name, returning it if it finds it in the state,
+      * or yielding None otherwise.
+      */
+    override def findByName(name: String): PersonOp[Option[Person]] =
+      State.inspect(_.personByName.get(name))
 
     /**
-     * Returns all the people in the state.
-     */
-    override def getAllPeople(): PersonOp[List[Person]] = ???
+      * Adds a person to the state.
+      */
+    override def addPerson(person: Person): PersonOp[Unit] =
+      State.modify(_.addPerson(person))
+
+    /**
+      * Returns all the people in the state.
+      */
+    override def getAllPeople(): PersonOp[List[Person]] =
+      State.inspect(_.personByName.values.toList)
   }
 }
