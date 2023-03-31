f39bd6255b0b2fc50ce42f66a48e4f36ddf9942e
Ujjal Dey <ujjaldey@gmail.com>
2023-03-31 18:22:16 +0800

    Video 107

--- src/main/scala/fpfinal/common/IO.scala
+++ src/main/scala/fpfinal/common/IO.scala
@@ -38,29 +38,40 @@ object IO {
     }
 
   @tailrec
-  final def resume[A](io: IO[A], eh: Option[ErrorHandler[A]]): Either[() => (IO[A], Option[ErrorHandler[A]]), A] =
+  final def resume[A](
+      io: IO[A],
+      eh: Option[ErrorHandler[A]]
+  ): Either[() => (IO[A], Option[ErrorHandler[A]]), A] =
     io match {
       case Done(a)       => Right(a)
       case More(thunk)   => resume(safeApply(thunk()), eh)
       case RaiseError(e) =>
         /**
-         * TODO #24: Implement the case where the computation represents an error.
-         *
-         * There are two posibilities:
-         * - We don't have a registered error handler (eh is None).
-         *     In this case we just throw the exception.
-         * - We have a register error handler (eh is Some)
-         *     In this case we apply the error handler on the error, and we resume the computation
-         *     without the error handler (i.e. an error handler can be consumed only once).
-         */
-        ???
-      case HandleErrorWith(ioa, newEh) => resume(ioa.asInstanceOf[IO[A]], Some(newEh.asInstanceOf[ErrorHandler[A]]))
+          * TODONE #24: Implement the case where the computation represents an error.
+          *
+          * There are two posibilities:
+          * - We don't have a registered error handler (eh is None).
+          *     In this case we just throw the exception.
+          * - We have a register error handler (eh is Some)
+          *     In this case we apply the error handler on the error, and we resume the computation
+          *     without the error handler (i.e. an error handler can be consumed only once).
+          */
+        eh match {
+          case Some(eha) => resume(eha(e), None)
+          case None      => throw e
+        }
+      case HandleErrorWith(ioa, newEh) =>
+        resume(
+          ioa.asInstanceOf[IO[A]],
+          Some(newEh.asInstanceOf[ErrorHandler[A]])
+        )
       case FlatMap(t, f) =>
         t match {
-          case Done(a2)        => resume(safeApply(f(a2)), eh)
-          case More(thunk2)    => Left(() => (FlatMap(safeApply(thunk2()), f), eh))
-          case RaiseError(e)   => resume(RaiseError(e), eh)
-          case HandleErrorWith(ioa, eh2) => resume(FlatMap(ioa, f), Some(eh2.asInstanceOf[ErrorHandler[A]]))
+          case Done(a2)      => resume(safeApply(f(a2)), eh)
+          case More(thunk2)  => Left(() => (FlatMap(safeApply(thunk2()), f), eh))
+          case RaiseError(e) => resume(RaiseError(e), eh)
+          case HandleErrorWith(ioa, eh2) =>
+            resume(FlatMap(ioa, f), Some(eh2.asInstanceOf[ErrorHandler[A]]))
           case FlatMap(t2, f2) =>
             resume(FlatMap(t2, (x: Any) => FlatMap(safeApply(f2(x)), f)), eh)
         }
@@ -74,20 +85,23 @@ object IO {
   def suspend[A](a: => A): IO[A] =
     More { () => Done(a) }
 
-  implicit val ioMonadError: MonadError[IO, Throwable] = new MonadError[IO, Throwable] {
-    override def pure[A](x: A): IO[A] = Done(x)
+  implicit val ioMonadError: MonadError[IO, Throwable] =
+    new MonadError[IO, Throwable] {
+      override def pure[A](x: A): IO[A] = Done(x)
 
-    override def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = FlatMap(fa, f)
+      override def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] =
+        FlatMap(fa, f)
 
-    override def tailRecM[A, B](a: A)(f: A => IO[Either[A, B]]): IO[B] = {
-      flatMap(f(a)) {
-        case Left(a)  => tailRecM(a)(f)
-        case Right(b) => pure(b)
+      override def tailRecM[A, B](a: A)(f: A => IO[Either[A, B]]): IO[B] = {
+        flatMap(f(a)) {
+          case Left(a)  => tailRecM(a)(f)
+          case Right(b) => pure(b)
+        }
       }
+
+      override def raiseError[A](e: Throwable): IO[A] = RaiseError(e)
+
+      override def handleErrorWith[A](fa: IO[A])(f: Throwable => IO[A]): IO[A] =
+        HandleErrorWith(fa, f)
     }
-
-    override def raiseError[A](e: Throwable): IO[A] = RaiseError(e)
-
-    override def handleErrorWith[A](fa: IO[A])(f: Throwable => IO[A]): IO[A] = HandleErrorWith(fa, f)
-  }
 }
